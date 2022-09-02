025e285f21f4e90bb5d9d6badc9735c9195bcd6c
boscochw_ubuntU_ideapad <bosco.chw@gmail.com>
2022-09-02 14:44:14 -0700

    fireworks on birthday

--- client/src/components/ShowHappyBirthday.tsx
+++ client/src/components/ShowHappyBirthday.tsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { useCallback } from "react";
+import Particles from "react-particles";
+import type { Engine } from "tsparticles-engine";
+import { loadFireworksPreset } from "tsparticles-preset-fireworks";
+import { happyBirthdayTextStyle } from "../styles/ShowHappyBirthdayStyles";
 
 export const ShowHappyBirthday: React.FC = () => {
-  return <div></div>
-}
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFireworksPreset(engine);
+  }, []);
+
+  return (
+    <>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        options={{ preset: ["fireworks"] }}
+      />
+      <div className={happyBirthdayTextStyle}>Happy Birthday!</div>
+    </>
+  );
+};
---
+++ client/src/styles/ShowHappyBirthdayStyles.ts
@@ -0,0 +1,9 @@
+import { style } from "typestyle";
+
+export const happyBirthdayTextStyle = style({
+  fontSize: "4rem",
+  fontFamily: "georgia",
+  zIndex: "10",
+  position: "relative",
+  color: "aliceblue"
+});
